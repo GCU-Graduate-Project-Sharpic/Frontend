b13942237aeb68f5d6defdcf7dbf0b3f02df4dfc
kaeunseong <84667083+kaeunseong@users.noreply.github.com>
2022-09-13 19:49:16 +0900

    Troubleshoot compilation problems

--- src/App.js
+++ src/App.js
@@ -1,64 +1,21 @@
-import React from 'react';
-import ImageUploader from './component/index.js';
+import React from "react";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Login from './component/Login';
+import Register from './component/Register';
+import Main from './Main';
 
-
-
-export default class App extends React.PureComponent {
-    render() {
-        return (
-            <div>
-                <div className="App">
-
-                    {/* header */}
-                    <div className="header">
-                        <img alt="setting" src="settings.png" />
-
-                        <h1 style={{ textAlign: "center" }}>Sharpic</h1>
-                    </div>
-
-
-                    <div className="main">
-
-                        <div className="imagetitle">
-                            <img style={{ width: "3vw", height: "3vw", marginLeft: "30px", marginRight: "10px", float: "left" }} alt="book" src="book.png" />
-                            <img id="share" style={{ width: "1.5vw", height: "1.5vw", marginTop: "5px", marginLeft: "10px", float: "right" }} alt="friend" src="friend.png" />
-                            <img style={{ width: "1.5vw", height: "1.5vw", marginTop: "5px", marginLeft: "10px", marginRight: "10px", float: "right" }} alt="share" src="share.png" />
-                            <img style={{ width: "1.5vw", height: "1.5vw", marginTop: "5px", marginLeft: "10px", marginRight: "10px", float: "right" }} alt="menu" src="menu.png" />
-                            <img style={{ width: "1.5vw", height: "1.5vw", marginTop: "5px", marginLeft: "10px", marginRight: "10px", float: "right" }} alt="menu2" src="menu2.png" />
-
-                            <h1 style={{ marginLeft: "40px", marginTop: "20px" }}>Seoul Travel</h1>
-                        </div>
-
-                        <div className='imgmenu'>
-                            <ImageUploader style={{ maxWidth: '800px', margin: "10px auto" }}
-                                withPreview={true} />
-                        </div>
-
-
-                    </div>
-
-
-                    <div className="profile">
-                        <img alt="Profile" src="user.png" />
-                        <div>
-                            <h1>James</h1>
-                            <h4 style={{}}>example@gmail.com</h4>
-                        </div>
-                    </div>
-
-
-                    <div className="menu">
-                        <img alt="book" src="book.png" />
-                        <h2 >Seoul Travel</h2>
-                        <img alt="book" src="book2.png" />
-                        <h2>Tokyo Travel</h2>
-                        <img alt="book" src="book2.png" />
-                        <h2>Busan Travel</h2>
-                    </div>
-
-                </div>
-            </div>
-
-        );
-    }
+function App() {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Main/>}/>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Register />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
+
+export default App;
---
+++ src/component/Register.js
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+import axios from 'axios';
+import "./Login.css";
+
+class Register extends Component{
+  state={
+    username:'',
+    password:'',
+    email: ''
+  }
+
+  appChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+  appClick = () => {
+    console.log(`username는 : ${this.state.username}\npw는 : ${this.state.password}\nemail은 : ${this.state.email}`);
+  }
+  appKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      this.appClick();
+    }
+  }
+  confirm=(e)=>{
+    axios
+      .post( // Login request
+        'http://localhost:8005/signup',
+        {
+          username: this.state.username,
+          password: this.state.password,
+          email: this.state.email
+        },
+        { 'Content-Type': 'application/json', withCredentials: true }
+      )
+      .then((res) => {
+        console.log(res.data)
+        if (res.data.status === 'signup success') {
+            window.location.replace("/")
+        } else {
+          console.log("wrong data");
+        }
+      })
+      .catch((err) => alert(err));
+  }
+  render() {
+    const { username, password, email } = this.state;
+    const { appChange, confirm, appKeyPress } = this;
+    return (
+      <div className="container-center-horizontal">
+        <div className="desktop-1 screen">
+          <h1 className="title">
+            Sharpic
+          </h1>
+          <input type="text" name="username" placeholder= "Email Address" value={username} onChange={appChange} className="component border-2px-black email-address valign-text-middle roboto-normal-black-15px"/>  
+          <input type="password" name="password" placeholder= "Password" value={password} onChange={appChange} className="overlap-group border-2px-black password valign-text-middle roboto-normal-black-15px"/>
+          <input type="text" name="email" placeholder= "Nickname" value={email} onChange={appChange} onKeyPress={appKeyPress} className="component border-2px-black email-address valign-text-middle roboto-normal-black-15px"/>  
+         <button onClick={confirm} className="button-1 log-in valign-text-middle">CONFIRM</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Register;
